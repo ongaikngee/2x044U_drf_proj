76aa1a8673fd4f8f2682a176c0678f9cda6881ff
Jonathan Ong <jonathan.ong@vainsight.com>
2024-01-22 13:19:06 +0800

    added root and highlighted endpoints

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls")),
-    path("", include("snippets.urls")),  # new
+    path("", include("snippets.urls")),  
 ]
